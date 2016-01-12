97ec913a9c3bf15a01bd7e0c1aba1f5cc0e7bda1
juanAFernandez <juan.antonio.fernandez.sanchez.88@gmail.com>
2016-01-12 17:29:02 +0100

    "Modificada aplicación en AngularJS de prueba y creada la base para la nuestra. Implementando la gestion de las vistas anidadas con **ui-router** , una librería que lo hace algo muy sencillo. Además se consumen datos desde la api aunque aún sin un formato correcto, aún. :)

--- SMS-Back-End/microservicio1/main.py
+++ SMS-Back-End/microservicio1/main.py
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
 import webapp2
+import json
+from tools.GestorAlumnosSQL import GestorAlumnos
 
 ## CLASE QUE PROCESA EL RECURSO /alumnos usando una forma estandar de uso  de parámetros.
 class Alumnos(webapp2.RequestHandler):
 
     def get(self):
-        self.response.write("Prueba modulo mobile-frontend")
+
+        #Se está pidiendo que se devuelvan todos los alumnos
+        listaAlumnos = GestorAlumnos.getAlumnos()
+
+
+        #Una vez que tenemos la lista de aĺumnos convertimos los datos a JSON para enviarlos
+        salida=""
+        for alumno in listaAlumnos:
+            salida+= str(json.dumps(alumno.__dict__))
+
+        #Los enviamos
+        self.response.write(salida)
 
 
 app = webapp2.WSGIApplication([
