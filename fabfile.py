78daa8ff9d2b764cb7c7cc0c7b8d4546020ef081
JA-Gonz <jagonccc@gmail.com>
2016-03-06 18:03:46 +0100

    Añadiendo primera aproximacion de fabfile #20

--- fabfile.py
+++ fabfile.py
@@ -16,27 +16,29 @@ def pararTodo():
     run ('kill -9 $(pidof python)')
 
 def lanzarTodo():
-
+    run ('./run')
 def lanzarServicioBD():
-
+    run ('google_appengine/dev_appserver.py --port=8001 --admin_port=8082 SMS-Back-End/microservicio1/microservicio1.yaml')
 def lanzarServicioControlEstdiantes():
+    run ('google_appengine/dev_appserver.py --port=8001 --admin_port=8082 SMS-Back-End/microservicio1/microservicio2.yaml')
 
-    pass
-def instalarDependencias():
+def instalarDependenciasServicioBD():
+    run ('sudo pip install -r SMS-Back-End/microservicio1/requirements.txt')
+def instalarDependenciasServicioControlEstudiantes():
+    run ('sudo pip install -r SMS-Back-End/microservicio2/requirements.txt')
+def instalarTodasDependencias():
+    run ('sudo ')
+     run ('sudo pip install -r SMS-Back-End/microservicio1/requirements.txt')
+     run ('sudo pip install -r SMS-Back-End/microservicio2/requirements.txt')
 
-    pass
 def obtenerLineasProyecto():
-
-    pass
+    run('echo \"Para contar las lineas del proyecto\"')
+    wc -l `find SMS-Front-End``find SMS-Back-End`
 def subirGAE():
-
-    pass
+    run ('google_appengine/appcfg.py -A studentsmanagementsystem-1124 update sms/')
 def actualizarProyecto():
-
-    pass
+    run ('git pull')
 def borrarProyecto():
-
-    pass
+    run ('rm -rf ./*')
 def test_APIBD():
-
-    pass
+    run ('python SMS-Back-End/microservicio1/APIDB/testUnitario.py')
