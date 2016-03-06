17b409e4eec3968cea52314d96dc2947460129a4
JA-Gonz <jagonccc@gmail.com>
2016-03-06 13:35:21 +0100

    Revirtiendo

--- requirements_bash.sh
+++ requirements_bash.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
 echo "Downloading the Google App Engine SDK"
 curl -O https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.30.zip
 echo "Unzip SDK"
 unzip google_appengine_1.9.30.zip 
 echo "Deleting .zip"
 rm google_appengine_1.9.30.zip 
+
+sudo
