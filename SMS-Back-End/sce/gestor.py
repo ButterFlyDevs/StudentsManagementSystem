ddb70e307d7a4a3af1aaa5ae1c7e1ba761bd2963
JA-Gonz <jagonccc@gmail.com>
2016-05-05 13:10:57 +0200

    Añadiendo implementaciones getallCA e insertar CA. Incluyendo key en las estructuras

--- SMS-Back-End/sce/gestor.py
+++ SMS-Back-End/sce/gestor.py
@@ -1,20 +1,35 @@
 from NDBlib import EstructurasNDB.ControlAsistencia
+from NDBlib import EstructurasNDB.Resumen_ControlAsistencia
+from NDBlib import EstructurasNDB.Alumnos_NombreID
+from NDBlib import EstructurasNDB.Profesores_NombreID
+from NDBlib import EstructurasNDB.Clases_NombreID
+from NDBlib import EstructurasNDB.Asignaturas_NombreID
 import Estructuras
 
 
 class Gestor(self):
 
     @classmethod
+    def obtenerALLCA():
+        listaCA = []
+        listaCA = ControlAsistencia.devolver_todo().fetch(100000)
+        
+    @classmethod
     def obtenerControlAsistencia(id):
         pass
 
     @classmethod
     def obtenerResumenControlAsistencia(idProfesor,idASignatura,idClase,fechaHora):
+'''
+Debe devolver también nombres (DEBE DEVOLVER UN RCA_complejo)
+'''
         pass
 
     @classmethod
     def insertarControlAsistencia(fechaHora,uniforme,rnj,rj,falta,idAlumno,idProfesor,idClase,idASignatura):
-        pass
+
+        nuevoCA = ControlAsistencia(fecha_hora = fechaHora,uniforme = uniforme,retraso_no_justificado =rnj ,retraso_justificado =rj ,falta = falta,id_alumno = idAlumno, id_profesor = idProfesor,id_clase =idClase , id_asignatura =idAsignatura)
+        nuevoCA_clave = nuevoCA.put()
 
     @classmethod
     def insertarResumenControlAsistencia(listaIdCA,fechaHora,idProfesor,idASignatura,idClase):
