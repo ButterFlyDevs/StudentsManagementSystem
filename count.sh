ec4ab1bce6856e49ae49f19de0c142f4dd62362a
juanAFernandez <juan.antonio.fernandez.sanchez.88@gmail.com>
2016-05-09 13:06:28 +0200

    Mejorando el contador

--- count.sh
+++ count.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
 
 #Contamos las lineas del mServicio apigateway
-APIG_lines="$(wc -l SMS-Back-End/apigateway/*.py SMS-Back-End/apigateway/*.txt SMS-Back-End/apigateway/*yaml | grep 'total' | cut -d' ' -f3)"
+APIG_name='APIG'
+APIG_folder='SMS-Back-End/apigateway/'
+APIG_lines2="$(wc -l $(find "$APIG_folder" ! -name *.pyc ! -name *.jpg ! -path 'SMS-Back-End/apigateway/lib/*' ! -wholename SMS-Back-End/apigateway/lib ! -wholename SMS-Back-End/apigateway/) | grep 'total' | awk '{print $1}')"
 
-git checkout gh-pages
 
-echo $APIG_lines >> lines.txt
+#Contamos las lineas del mServicio Base de Datos
+mSBD_name='mSBD'
+mSBD_lines="$(wc -l SMS-Back-End/microservicio1/*.py SMS-Back-End/microservicio1/*.txt SMS-Back-End/microservicio1/*.yaml | grep 'total' | cut -d' ' -f2)"
 
-git commit -am "Prueba de escritura"
+#Contamos las lineas del mServicio Control Estudiantes
+mSCE_name='mSCE'
+mSCE_lines="$(wc -l SMS-Back-End/sce/*.py SMS-Back-End/sce/*.txt SMS-Back-End/sce/*.yaml | grep 'total' | cut -d' ' -f2)"
 
-git checkout master
+#Número del commit:
+commit_number="$(git rev-list --count HEAD)"
+
+#Vamos a guardar una linea separándola con ';'
+file_line=$APIG_name";"$APIG_lines2";"$mSBD_name";"$mSBD_lines";"$mSCE_name";"$mSCE_lines";"$commit_number";"$(date +"%d-%m-%Y")
+
+echo $file_line
+
+
+#Después realizamos la escritura en un fichero de la rama gh-pages para que se muestre el gráfico en la web.
+
+#git checkout gh-pages
+
+#echo $APIG_lines >> lines.txt
+
+#git commit -am "Prueba de escritura"
+
+#git checkout master
